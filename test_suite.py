d1509937eb0fd57d269237261efeb9a32f7013f2
mwwang7 <mwwang@usc.edu>
2023-10-19 20:44:54 -0700

    Skeleton code for test suite

---
+++ test_suite.py
@@ -0,0 +1,20 @@
+# Team Members: Michael Wang
+# Github Repo Link: 
+"""EE 250L Lab 07
+
+Run test_suite.py on Raspberry Pi."""
+
+# Imports
+
+if __name__ == '__main__':
+  while True:
+    # Blink the LED 5 times with on/off intervals of 500ms
+
+    # Read output of Grove light sensor for 5s with intervals of 100ms
+    # Print raw values of light sensor with text "bright" or "dark"
+
+    # Blink the LED 4 times with on/off intervals of 200ms
+
+    # Read output of Grove sound sensor for 5s with intervals of 100ms
+    # Print sound sensor raw value
+    # If sound sensor is tapped, turn on LED for 100ms
