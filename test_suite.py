a8d72080c5b83e44a927197de6b08d95601c65da
mwwang7 <mwwang@usc.edu>
2023-10-19 23:56:34 -0700

    Changed SPI congfig to hardware

--- test_suite.py
+++ test_suite.py
@@ -25,12 +25,10 @@ light_sensor_pin = 1
 GPIO.setmode(GPIO.BOARD)
 # Set led_pin to be an output pin
 GPIO.setup(led_pin, GPIO.OUT)
-# Software SPI configuration:
-SCLK = 23
-MISO = 21
-MOSI = 19
-CE0 = 24
-mcp = Adafruit_MCP3008.MCP3008(clk=SCLK, cs=CE0, miso=MISO, mosi=MOSI)
+# Hardware SPI configuration:
+SPI_PORT   = 0
+SPI_DEVICE = 0
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
 if __name__ == '__main__':
   while True:
